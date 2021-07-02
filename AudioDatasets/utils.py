818d9c8a91fd62758d38720b749c284594c515d2
KinWaiCheuk <u3500684@connect.hku.hk>
2021-07-02 20:12:39 +0800

    add tsv2roll function

---
+++ AudioDatasets/utils.py
@@ -0,0 +1,42 @@
+import torch
+import numpy as np
+
+def tsv2roll(tsv, audio_length, sample_rate, hop_size, max_midi, min_midi):
+    """
+    Converting a tsv file into a piano roll
+    
+    Parameters
+    ----------
+    tsv : numpy.ndarray
+        The tsv label in the array format
+    hop_size: int
+        Hop size for the spectrogram. It will be used to convert tsvs into piano rolls
+    max_midi: int
+        The top bin of the pianoroll. Default 108 which corresponds to C8, the highest key on a standard 88-key piano
+    min_midi: int
+        The lowest bin of the pianoroll. Default 21 which corresponds to A0, the lowest key on a standard 88-key piano
+    """
+    
+    
+    n_keys = max_midi - min_midi + 1 # Calutate number of bins for the piano roll
+    n_steps = (audio_length - 1) // hop_size + 1 # Calulate number of timesteps for the piano roll
+    
+    pianoroll = np.zeros((n_steps, n_keys), dtype=int)
+    velocity_roll = np.zeros((n_steps, n_keys), dtype=int)
+    
+    for onset, offset, note, vel in tsv:
+        left = int(round(onset * sample_rate / hop_size)) # Convert time to time step
+        onset_right = min(n_steps, left + 1) # Ensure the time step of onset would not exceed the last time step
+        frame_right = int(round(offset * sample_rate / hop_size))
+        frame_right = min(n_steps, frame_right) # Ensure the time step of frame would not exceed the last time step
+        offset_right = min(n_steps, frame_right + 1)
+
+        f = int(note) - min_midi
+        pianoroll[left:onset_right, f] = 3 # assigning onset
+        pianoroll[onset_right:frame_right, f] = 2 # assigning sustain
+        pianoroll[frame_right:offset_right, f] = 1 # assigning offset
+        velocity_roll[left:frame_right, f] = vel    
+        
+        
+        
+    return pianoroll, velocity_roll
